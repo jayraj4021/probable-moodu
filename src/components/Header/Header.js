845b81aa26d565c11efb40cf3902ea741b91b863
jayraj4021 <jayraj4021@gmail.com>
2021-08-13 16:55:01 +0530

    added menu in mobile view

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import {React, useState} from 'react';
 import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { supabase } from '../../supabaseClient';
 import { Link} from 'react-router-dom';
@@ -9,6 +12,54 @@ import './Header.css'
 export default function Header({session,setSession,history}) {
 
   const widthCheckQuery = useMediaQuery('(min-width:600px)');
+  
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [compToShow, setCompToShow] = useState(0);
+
+  const menuItems = [
+    {id:'0', name:'Profile'},
+    {id:'1', name:'Make an entry'},
+    {id:'2', name:'Previous Entries'},
+    {id:'3', name:'Logout'},
+  ]
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = (event) => {
+    setAnchorEl(null);
+    console.log(event.target.textContent)
+  };
+
+  const StyledMenu = withStyles({
+    paper: {
+      backgroundColor: '#00ADB5',
+      color:'white'
+    },
+  })((props) => (
+    <Menu
+      elevation={0}
+      getContentAnchorEl={null}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+      {...props}
+    />
+  ));
+  
+  const StyledMenuItem = withStyles((theme) => ({
+    root: {
+      '&:focus': {
+        backgroundColor: theme.palette.primary.main,
+      },
+    },
+  }))(MenuItem);
 
   let toolBarClass = 'toolBar';
   if(widthCheckQuery){
@@ -36,6 +87,41 @@ export default function Header({session,setSession,history}) {
             </Typography>
           </Link>
          {session ? 
+            <div style={{display:'flex'}}>
+              <div style={{marginRight:'20px'}}>
+                <Button aria-controls="simple-menu" aria-haspopup="true"
+                  onClick={handleClick} 
+                  variant="contained"
+                  color="secondary" 
+                  size='small'
+                  style={{color:'white'}}
+                  >
+                  Menu
+                </Button>
+                <StyledMenu
+                  id="simple-menu"
+                  anchorEl={anchorEl}
+                  keepMounted
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                    {menuItems.map(el=>(
+                        <StyledMenuItem onClick={handleClose} 
+                        selected={el.id===0}>
+                          {el.name}
+                        </StyledMenuItem>
+                    ))}
+                    
+
+                    {/* <StyledMenuItem onClick={handleClose} 
+                    selected={compToShow===1}>
+                      Previous Notes
+                    </StyledMenuItem>
+                    <StyledMenuItem onClick={handleClose} >
+                      Logout
+                    </StyledMenuItem>*/}
+                </StyledMenu> 
+              </div>
             <Button variant="contained" 
                     color="secondary" 
                     size='small' 
@@ -46,6 +132,7 @@ export default function Header({session,setSession,history}) {
                     style={{color:'white'}} >
                       Sign Out
             </Button>
+          </div>
          :
           <Link to="/signin">
             <Button variant="contained" 
