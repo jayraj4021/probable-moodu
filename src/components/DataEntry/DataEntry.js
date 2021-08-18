606b190e171849f0f46989bc7c618398cb14e2c3
jayraj4021 <jayraj4021@gmail.com>
2021-08-18 21:26:02 +0530

    added toasts and info messages

--- src/components/DataEntry/DataEntry.js
+++ src/components/DataEntry/DataEntry.js
@@ -1,6 +1,14 @@
 import {React,useState} from 'react';
 import Button from '@material-ui/core/Button';
 import { supabase } from '../../supabaseClient';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import Tooltip from '@material-ui/core/Tooltip';
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
 
 const DataEntry = () => {
 
@@ -9,26 +17,53 @@ const DataEntry = () => {
   const [todaysNote, setTodaysNote] = useState('')
   const [loading, setLoading] = useState(false)
 
+  const [openSuccess, setOpenSuccess] = useState(false)
+  const [openError, setOpenError] = useState(false)
+
   const currentUser = supabase.auth.user()
   //console.log(currentUser)
 
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpenSuccess(false);
+    setOpenError(false);
+  };
+
   const handleSubmit = async () => {
-        const { data, error } = await supabase
-        .from('data_table')
-        .insert([
-          {   rating,
-              mood,
-              'body':todaysNote,
-              created_date:new Date().toISOString().slice(0, 10),
-              creator_id:currentUser.id
-           },
-        ])
+      try{
+            setLoading(true);
+            const { data, error } = await supabase
+            .from('data_table')
+            .insert([
+            {   rating,
+                mood,
+                'body':todaysNote,
+                created_date:new Date().toISOString().slice(0, 10),
+                creator_id:currentUser.id
+            },
+            ]);
+            if(error) throw error;
+            setOpenSuccess(true);
+        } catch(error) {
+            setOpenError(true);
+        } finally {
+            setLoading(false);
+        }
   }
 
   return (
     <div style={{width:'100%',backgroundColor:'#393E46',borderRadius:'5px',padding:'20px',
                 display:'flex',alignItems:'flex-start',flexDirection:'column',fontFamily:'Open Sans, sans-serif'}}>
-        <p>Select Today's Rating</p>
+        <div style={{display:'flex'}}>
+            <p style={{paddingRight:'5px'}}>Select Today's Rating </p>
+            <Tooltip 
+                title="Select rating between 0 (worst day) and 10 (best day)" 
+                placement="top" arrow>
+                <InfoOutlinedIcon style={{opacity:'0.5'}}/>
+            </Tooltip>
+        </div>
         <input
             type="number"
             placeholder="Your Rating"
@@ -38,7 +73,15 @@ const DataEntry = () => {
             onChange={(e) => setRating(e.target.value)}
             style={{width:'100%', height:'30px',marginBottom:'25px', paddingLeft:'10px'}}
         />
-        <p>Enter Today's Mood</p>
+
+        <div style={{display:'flex'}}>
+            <p style={{paddingRight:'5px'}}>Enter Today's Mood </p>
+            <Tooltip 
+                title="Enter a mood describing your day" 
+                placement="top" arrow>
+                <InfoOutlinedIcon style={{opacity:'0.5'}}/>
+            </Tooltip>
+        </div>
         <input
             type="test"
             placeholder="Your dominant mood for the day"
@@ -46,7 +89,15 @@ const DataEntry = () => {
             onChange={(e) => setMood(e.target.value)}
             style={{width:'100%', height:'30px',marginBottom:'25px', paddingLeft:'10px'}}
         />
-        <p>Enter Today's Note</p>
+
+        <div style={{display:'flex'}}>
+            <p style={{paddingRight:'5px'}}> Enter Today's Note </p>
+            <Tooltip 
+                title="Describe how was your day " 
+                placement="top" arrow>
+                <InfoOutlinedIcon style={{opacity:'0.5'}}/>
+            </Tooltip>
+        </div>
         <textarea
             placeholder="Please unload your thoughts ...."
             value={todaysNote}
@@ -60,13 +111,23 @@ const DataEntry = () => {
                   size='small' 
                   onClick={(e) => {
                       e.preventDefault()
-                      handleSubmit(rating,mood,todaysNote)
+                      handleSubmit(rating,mood,todaysNote);
                   }}
                   disabled={loading}
                   style={{color:'white', width:'100%'}}>
                       {loading ? <span>Submitting ...</span> : <span>Submit</span>}
               </Button>
           </div>
+          <Snackbar open={openSuccess} autoHideDuration={3000} onClose={handleClose}>
+            <Alert onClose={handleClose} severity="success">
+                Submitted Successfully !!!
+            </Alert>
+          </Snackbar>
+          <Snackbar open={openError} autoHideDuration={3000} onClose={handleClose}>
+            <Alert onClose={handleClose} severity="error">
+                Something went wrong !!!
+            </Alert>
+          </Snackbar>
     </div>
   )
 }
